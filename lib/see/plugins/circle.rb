e2ede9de56f8f193379ed103ee4f6301a1dd24a9
Michael Avila <me@michaelavila.com>
2014-02-22 19:55:26 +0000

    Refactor circle plugin

--- lib/see/plugins/circle.rb
+++ lib/see/plugins/circle.rb
@@ -12,39 +12,43 @@ module See
         'circle'
       end
 
-      def run(config, plugin_config)
-        lines = []
-
+      def latest_builds(account, repository, lines)
         token = access_token('CIRCLE_CI_ACCESS_TOKEN')
         CircleCi.configure do |cfg|
           cfg.token = token
         end
 
-        response = CircleCi::Project.recent_builds(plugin_config['account'], plugin_config['repository'])
+        response = CircleCi::Project.recent_builds(account, repository)
         if response.errors.length > 0
           lines << "  Errors encountered:".red
           response.errors.each do |error|
             lines << "    - " + "Error #{error.code}:".red + " #{JSON.parse(error.message)['message'].strip}"
+            raise
           end
+        end
+
+        response.body[0..4]
+      end
+
+      def run(config, plugin_config)
+        lines = []
+
+        begin
+          builds = latest_builds(plugin_config['account'], plugin_config['repository'], lines)
+        rescue => error
           return lines
         end
 
         lines << "  Latest Builds:".light_blue
-        response.body[0..4].each do |thing|
-          if thing['committer_date']
-            time = "- #{Date.parse(thing['committer_date']).strftime("%b %e,%l:%M %p")}".black
-          end
-          if thing['status'].match(/failed|not_run/)
-            status = thing['status'].red
-          else
-            status = thing['status'].green
-          end
-          if thing['committer_name']
-            name = "[#{thing['committer_name']}]".cyan
-          else
-            name = ""
-          end
-          lines << "    - #{status.capitalize} #{thing["vcs_revision"][0..8].light_yellow} #{("#"+thing['build_num'].to_s).light_green} #{thing['subject']} #{name} #{time}"
+        lines << builds.map do |build|
+          time = "- #{Date.parse(build['committer_date']).strftime("%b %e,%l:%M %p")}".black if build['committer_date'] 
+          was_bad = build['status'].match(/failed|not_run/)
+          status = build['status'].capitalize.send(was_bad ? :red : :green)
+          name = "[#{build['committer_name']}]".cyan ||= ''
+          commit_id = build["vcs_revision"][0..8].light_yellow
+          build_number = ("#"+build['build_num'].to_s).light_green
+
+          "    - #{status} #{commit_id} #{build_number} #{build['subject']} #{name} #{time}"
         end
         lines
       end
