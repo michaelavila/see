1404428fa65b34851a541145bb7af76ff3a9cee2
Michael Avila <me@michaelavila.com>
2014-02-22 19:13:17 +0000

    Simplify run_plugin

--- lib/see/plugins.rb
+++ lib/see/plugins.rb
@@ -2,31 +2,31 @@ require 'colorize'
 
 module See
   module Plugins
-    def self.run_plugin(name, config)
-      plugins = See::Plugins.constants.select do |const|
+    def self.find_plugins(name)
+      See::Plugins.constants.select do |const|
         const != :Plugin
       end.map do |const|
         See::Plugins.const_get(const).new
       end.select do |plugin|
         plugin.config_name == name
       end
+    end
 
-      lines = []
+    def self.run_plugin(name, config)
+      plugins = find_plugins(name)
       if plugins.empty?
-        lines << "\nNo plugin found with the name \"#{name}\"".light_red
-      else
-        plugins.each do |plugin|
-          lines << "\n"
-          lines << plugin.display_name.light_magenta
-          begin
-            lines_from_plugin = plugin.run(config, config[plugin.config_name])
-          rescue => error
-            lines_from_plugin = ["  #{error.message}".red]
-          end
-          lines.concat(lines_from_plugin)
+        return ["\nNo plugin found with the name \"#{name}\"".light_red]
+      end
+
+      lines = ["\n"]
+      lines << plugins.map do |plugin|
+        lines_from_plugin = [plugin.display_name.light_magenta]
+        begin
+          lines_from_plugin << plugin.run(config, config[plugin.config_name])
+        rescue => error
+          lines_from_plugin << ["  #{error.message}".red]
         end
       end
-      lines
     end
 
     class Plugin
